3f36aea4aa0b550fde05fd1f4a2437fd99f8790a
Karan Kakati <karanpkofficial@gmail.com>
2022-05-31 15:19:05 +0530

    whitenoise addition

--- procostaMain/settings.py
+++ procostaMain/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definitions
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -51,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'procostaMain.urls'
