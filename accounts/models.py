9b3665f8430257cbd1a42cde80c88cc2dc151e78
Karan Kakati <karanpkofficial@gmail.com>
2022-06-15 21:26:57 +0530

    added int for phases set

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,8 @@
 from cmath import phase
 from pyexpat import model
 from django.db import models
+from django.forms import ValidationError
 from sqlalchemy import delete
-
-
-
 
 class Project(models.Model):
     STATUS = (
@@ -16,16 +14,16 @@ class Project(models.Model):
     )
 
     PHASE = (
-        ('1', '1'),
-        ('2', '2'),
-        ('3', '3'),
-        ('4', '4'),
-        ('5', '5'),
-        ('6', '6'),
-        ('7', '7'),
-        ('8', '8'),
-        ('9', '9'),
-        ('10', '10')
+        (1,"1"),
+        (2,"2"),
+        (3,"3"),
+        (4,"4"),
+        (5,"5"),
+        (6,"6"),
+        (7,"7"),
+        (8,"8"),
+        (9,"9"),
+        (10,"10")
     )
 
     p_id = models.IntegerField(null=True)
@@ -36,7 +34,11 @@ class Project(models.Model):
     ect = models.DateField(null=True)
     status = models.CharField(max_length=200,null=True, choices=STATUS)
     collabs = models.IntegerField(null=True)
-    phase = models.IntegerField(null=True, choices=PHASE)
+    phase = models.CharField(null=True, max_length=100 ,choices=PHASE)
     
     def __str__(self):
         return self.p_name
+
+    @property
+    def phase_percentage(self):
+        return self.phase * 10
