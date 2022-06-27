3335530fa04378555b175ba51b36a2b7600c2175
Karan Kakati <karanpkofficial@gmail.com>
2022-06-27 22:16:35 +0530

    added new library add on - Crisp

--- procostaMain/settings.py
+++ procostaMain/settings.py
@@ -41,11 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 
     # main app folder 
     'accounts',
     # 'send',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
