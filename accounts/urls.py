18fcd670cca69f4de27dd8e7bd1f560968e9f787
Karan Kakati <karanpkofficial@gmail.com>
2022-06-27 23:05:13 +0530

    added login & reg path

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register', views.register, name="register"),
+    path('login', views.login, name="login"),
+
+
     path('', views.dashboard, name="dashboard"),
     path('dashboard/', views.dashboard, name="dashboard"),
     path('status/<str:no>/', views.status, name="status"),
+
     path('addProject/', views.addProject, name="addProject"),
     path('updateProject/<str:no>/', views.updateProject, name="updateProject"),
     path('deleteProject/<str:no>/', views.deleteProject, name="deleteProject"),
