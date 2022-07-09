01857a26ee844aa5b7609ad6d4d60d4ec08d2ce1
Karan Kakati <karanpkofficial@gmail.com>
2022-07-10 01:13:07 +0530

    email_changes

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,8 @@
+import email
+import profile
+from re import template
 from django.shortcuts import render, redirect
+from flask import render_template, render_template_string
 from .models import Project
 from django.shortcuts import get_object_or_404
 from .models import *
@@ -10,6 +14,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 # views
+
+
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
+
 def registerPage(request):
     if request.user.is_authenticated:
         return redirect('dashboard')
@@ -17,14 +27,51 @@ def registerPage(request):
         form = CreateUserForm()
 
         if request.method == 'POST':
+            print('Printing post:', request.POST)
             form = CreateUserForm(request.POST)
             if form.is_valid():
                 form.save()
 
                 fname = form.cleaned_data.get('first_name')
+                reg_email = form.cleaned_data.get('email')
+                reg_username = form.cleaned_data.get('username')
                 messages.success(request, 'Account created. Welcome, ' + fname + '!')
 
+                # send mail to confirm the user of his successful registration
+                template = render_to_string('accounts/onboard.html', {'fname':fname, 'reg_email':reg_email, 'reg_username':reg_username})
+                
+                # from templated_email import get_templated_mail
+
+                # get_templated_mail(
+                #     template_name='Welcome aboard' + ' ' + fname + '!',
+                #     from_email=settings.EMAIL_HOST_USER,
+                #     to=[reg_email],
+                #     context={
+                #         'reg_username':request.user.username,
+                #         'fname':request.user.first_name,
+                #         'reg_email':request.user.email
+                #     },
+                #     # Optional:
+                #     # cc=['cc@example.com'],
+                #     # bcc=['bcc@example.com'],
+                #     # headers={'My-Custom-Header':'Custom Value'},
+                #     # template_prefix="my_emails/",
+                #     # template_suffix="email",
+                # )
+
+                email = EmailMessage(
+                    'Welcome aboard' + ' ' + fname + '!',
+                    template,
+                    settings.EMAIL_HOST_USER,
+                    [reg_email],
+                )
+
+                email.fail_silently = False
+                email.send()
+
                 return redirect('login')
+
+    
 
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
@@ -55,7 +102,7 @@ def logoutUser(request):
 def confirmLogout(request):
     return render(request, 'prompt/logoutUser.html')
 
-# @login_required(login_url='login')
+@login_required(login_url='login')
 def dashboard(request):
     projects = Project.objects.all()
     return render(request, 'accounts/dashboard.html', {'projects':projects})
@@ -64,7 +111,7 @@ def status(request, no):
     projects = get_object_or_404(Project, pk=no) 
     return render(request, 'accounts/status.html',{'projects':projects})
 
-# @login_required(login_url='login')
+@login_required(login_url='login')
 def addProject(request): 
     form = ProjectForm()
     if request.method == 'POST':
@@ -79,7 +126,7 @@ def addProject(request):
     context = {'form':form}
     return render(request, 'forms/addProject.html', context)
 
-# @login_required(login_url='login')
+@login_required(login_url='login')
 def updateProject(request, no):
     proj = Project.objects.get(id=no)
     form  = ProjectForm(instance=proj)
@@ -94,7 +141,7 @@ def updateProject(request, no):
     context = {'form':form}
     return render(request, 'forms/updateProject.html', context)
 
-# @login_required(login_url='login')
+@login_required(login_url='login')
 def deleteProject(request, no):
     proj = Project.objects.get(id=no)
 
